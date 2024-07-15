60f04a5f1a2addf6f49b154f74090e4d5cf4716a
aeueoe <drom.jul@gmail.com>
2024-07-15 12:47:16 +0200

    Fetch movies from API and use PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropType from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,33 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string,
+      Death: PropTypes.string,
+    }).isRequired,
+    ReleaseYear: PropTypes.number.isRequired,
+    IMDb_Rating: PropTypes.string.isRequired,
+    RottenTomatoesRating: PropTypes.string.isRequired,
+    Actors: PropTypes.arrayOf(
+      PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Character: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
